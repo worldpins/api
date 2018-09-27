040b0c6a71385ec7150633de3082099d92cb35c2
jdecroock <decroockjovi@gmail.com>
2018-09-27 15:50:06 +0200

    abstract out a few things

---
+++ src/graphql/helpers.js
@@ -0,0 +1,29 @@
+const { getAuthToken, tokenRefresher } = require('../web/authMiddleware');
+const { verifyAuthToken } = require('../utils/tokenizer');
+
+exports.authMiddleware = async ({ ctx }) => {
+  const { request: { header: headers } } = ctx;
+  let token = getAuthToken(headers.authorization || '');
+  if (!token) {
+    return { token };
+  }
+  let decodedToken;
+  try {
+    decodedToken = await verifyAuthToken(token);
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      token = await tokenRefresher(token);
+      decodedToken = await verifyAuthToken(token);
+    }
+  }
+  return { token: decodedToken, undecodedToken: token };
+};
+
+exports.formatError = (log, error) => {
+  const { stacktrace } = error.extensions.exception;
+  // Remove the verbose "Error:" prefix.
+  stacktrace.shift();
+  // Log it so we can see it when a user has an error.
+  log.error(`${(error.extensions && error.extensions.code) || 'Internal Server Error: '}: ${error.message}\n${stacktrace.join('\n')}`);
+  return error;
+};
--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,8 +2,7 @@ const Koa = require('koa');
 const { ApolloServer } = require('apollo-server-koa');
 
 const { makeLogger } = require('../utils/logger');
-const { getAuthToken, tokenRefresher } = require('../web/authMiddleware');
-const { verifyAuthToken } = require('../utils/tokenizer');
+const { authMiddleware, formatError } = require('./helpers');
 const makeSchema = require('./schema');
 
 const log = makeLogger('GraphQL Server');
@@ -12,31 +11,8 @@ async function makeServer() {
   log.info('Setting up apollo server.');
   const schema = makeSchema();
   const server = new ApolloServer({
-    context: async ({ ctx }) => {
-      const { request: { header: headers } } = ctx;
-      let token = getAuthToken(headers.authorization || '');
-      if (!token) {
-        return { token };
-      }
-      let decodedToken;
-      try {
-        decodedToken = await verifyAuthToken(token);
-      } catch (err) {
-        if (err.name === 'TokenExpiredError') {
-          token = await tokenRefresher(token);
-          decodedToken = await verifyAuthToken(token);
-        }
-      }
-      return { token: decodedToken, undecodedToken: token };
-    },
-    formatError: (error) => {
-      const { stacktrace } = error.extensions.exception;
-      // Remove the verbose "Error:" prefix.
-      stacktrace.shift();
-      // Log it so we can see it when a user has an error.
-      log.error(`${(error.extensions && error.extensions.code) || 'Internal Server Error: '}: ${error.message}\n${stacktrace.join('\n')}`);
-      return error;
-    },
+    context: authMiddleware,
+    formatError: formatError.bind(this, log),
     schema,
   });
   const app = new Koa();
