2f03f31d00b66e80cef2062c0c6695b5ecd6fc41
BrianPinsard <brian.pinsard@gmail.com>
2018-11-27 19:36:47 +0100

    pins migration fix

--- src/data/migrations/20180928095800_pins.js
+++ src/data/migrations/20180928095800_pins.js
@@ -6,11 +6,11 @@ exports.up = function up(knex) {
       table.text('comment');
       table.specificType('initial_area', 'POINT');
       table.timestamp('created_on').comment('Date the user was created.');
-      table.string('created_by').references('id').inTable('users');
+      table.uuid('created_by').references('id').inTable('users');
       table.timestamp('updated_on').comment('Date the user was last updated.');
-      table.string('updated_by').references('id').inTable('users');
+      table.uuid('updated_by').references('id').inTable('users');
       table.timestamp('deleted_on').comment('Date the user was soft-deleted.');
-      table.string('deleted_by').references('id').inTable('users');
+      table.uuid('deleted_by').references('id').inTable('users');
     })
     .createTable('pins', (table) => {
       table.uuid('id').primary().comment('Primary (id) entails an index.');
@@ -18,11 +18,11 @@ exports.up = function up(knex) {
       table.text('comment');
       table.specificType('coordinates', 'POINT');
       table.timestamp('created_on').comment('Date the user was created.');
-      table.string('created_by').references('id').inTable('users');
+      table.uuid('created_by').references('id').inTable('users');
       table.timestamp('updated_on').comment('Date the user was last updated.');
-      table.string('updated_by').references('id').inTable('users');
+      table.uuid('updated_by').references('id').inTable('users');
       table.timestamp('deleted_on').comment('Date the user was soft-deleted.');
-      table.string('deleted_by').references('id').inTable('users');
+      table.uuid('deleted_by').references('id').inTable('users');
       table.uuid('map_id').references('id').inTable('maps');
     })
     .createTable('userHasMaps', (table) => {
