5a25aa2df7a614f4e3137d7090ab5d1240aa1220
jdecroock <decroockjovi@gmail.com>
2019-03-17 10:23:33 +0100

    feat: publishing maps

--- src/services/mapService.js
+++ src/services/mapService.js
@@ -51,23 +51,28 @@ class MapService {
     if (!decodedToken) {
       throw new AuthenticationError('Must authenticate.');
     }
+    try {
+      const map = await this.dataController('maps')
+        .select('id', 'name', 'comment', 'initial_area as initialArea', 'published')
+        .where('id', id)
+        .first();
 
-    const map = await this.dataController('maps')
-      .select('id', 'name', 'comment', 'initial_area as initialArea')
-      .where('id', id)
-      .first();
+      if (!map) {
+        throw new ApolloError(`map with id "${id}" can't be found.`, MAP_NOT_FOUND);
+      }
 
-    if (!map) {
-      throw new ApolloError(`map with id "${id}" can't be found.`, MAP_NOT_FOUND);
+      // Default to Brussels.
+      const { initialArea = { x: 4.34878, y: 50.85045 }, ...rest } = map;
+      let area = initialArea;
+      if (!initialArea) {
+        area = { x: 4.34878, y: 50.85045 };
+      }
+      return { ...rest, initialArea: { longitude: area.x, latitude: area.y } };
+    } catch (e) {
+      logger.warn(e);
+      throw e;
     }
 
-    // Default to Brussels.
-    const { initialArea = { x: 4.34878, y: 50.85045 }, ...rest } = map;
-    let area = initialArea;
-    if (!initialArea) {
-      area = { x: 4.34878, y: 50.85045 };
-    }
-    return { ...rest, initialArea: { longitude: area.x, latitude: area.y } };
   }
 
   async uploadMap(map, decodedToken) {
@@ -123,67 +128,81 @@ class MapService {
     return mapId;
   }
 
+  async publishMap({ id, published }, decodedToken) {
+    if (!decodedToken) {
+      throw new AuthenticationError('Must authenticate.');
+    }
+
+    await this.dataController('maps').update('published', published).where('id', id);
+
+    return await this.getMap(id, decodedToken);
+  }
+
   async getMaps({
     from = 0, limit = 10, searchField, search, sortField, sortDirection,
   }, decodedToken) {
     if (!decodedToken) {
       throw new AuthenticationError('Must authenticate.');
     }
+    try {
+      let filteredCount;
 
-    let filteredCount;
+      const query = this.dataController('maps')
+        .select('maps.id', 'maps.name', 'maps.comment', 'maps.initial_area AS initialArea', 'published')
+        .count('pins.id AS amountOfPins')
+        .groupBy('maps.id')
+        .offset(from)
+        .limit(limit)
+        .orderBy('name')
+        .where('userHasMaps.user_id', decodedToken.userId)
+        .leftOuterJoin('pins', 'maps.id', 'pins.map_id')
+        .innerJoin('userHasMaps', 'maps.id', 'userHasMaps.map_id');
 
-    const query = this.dataController('maps')
-      .select('maps.id', 'maps.name', 'maps.comment', 'maps.initial_area AS initialArea')
-      .count('pins.id AS amountOfPins')
-      .groupBy('maps.id')
-      .offset(from)
-      .limit(limit)
-      .orderBy('name')
-      .where('userHasMaps.user_id', decodedToken.userId)
-      .leftOuterJoin('pins', 'maps.id', 'pins.map_id')
-      .innerJoin('userHasMaps', 'maps.id', 'userHasMaps.map_id');
-
-    if (searchField && search) {
-      if (!mapFields.includes(searchField)) {
-        throw new UserInputError(`The field ${searchField} does not exist in maps.`);
+      if (searchField && search) {
+        if (!mapFields.includes(searchField)) {
+          throw new UserInputError(`The field ${searchField} does not exist in maps.`);
+        }
+        query.andWhere(searchField, 'like', `%${search}%`);
+        const { count } = await this.dataController('maps')
+          .count('maps.id')
+          .where('userHasMaps.user_id', decodedToken.userId)
+          .andWhere(searchField, 'like', `%${search}%`)
+          .innerJoin('userHasMaps', 'maps.id', 'userHasMaps.map_id');
+        filteredCount = count;
       }
-      query.andWhere(searchField, 'like', `%${search}%`);
-      const { count } = await this.dataController('maps')
+
+      if (sortField && sortDirection) {
+        if (!mapFields.includes(sortField)) {
+          throw new UserInputError(`The field ${searchField} does not exist in maps.`);
+        }
+        query.orderBy(sortField, sortDirection);
+      }
+
+      logger.info(`Getting maps for ${decodedToken.userId}.`);
+      const maps = await query;
+
+      const { count: totalCount = 0 } = await this.dataController('maps')
         .count('maps.id')
         .where('userHasMaps.user_id', decodedToken.userId)
-        .andWhere(searchField, 'like', `%${search}%`)
         .innerJoin('userHasMaps', 'maps.id', 'userHasMaps.map_id');
-      filteredCount = count;
+      return {
+        items: maps.map(({ initialArea, ...rest }) => {
+          let area = initialArea;
+          if (!initialArea) {
+            area = { x: 4.34878, y: 50.85045 };
+          }
+          return {
+            ...rest,
+            initialArea: { longitude: area.x, latitude: area.y },
+          };
+        }),
+        totalCount,
+        filteredCount: filteredCount || totalCount,
+      };
+    } catch (e) {
+      logger.warn(e);
+      throw e;
     }
-
-    if (sortField && sortDirection) {
-      if (!mapFields.includes(sortField)) {
-        throw new UserInputError(`The field ${searchField} does not exist in maps.`);
-      }
-      query.orderBy(sortField, sortDirection);
-    }
-
-    logger.info(`Getting maps for ${decodedToken.userId}.`);
-    const maps = await query;
-
-    const { count: totalCount = 0 } = await this.dataController('maps')
-      .count('maps.id')
-      .where('userHasMaps.user_id', decodedToken.userId)
-      .innerJoin('userHasMaps', 'maps.id', 'userHasMaps.map_id');
-    return {
-      items: maps.map(({ initialArea, ...rest }) => {
-        let area = initialArea;
-        if (!initialArea) {
-          area = { x: 4.34878, y: 50.85045 };
-        }
-        return {
-          ...rest,
-          initialArea: { longitude: area.x, latitude: area.y },
-        };
-      }),
-      totalCount,
-      filteredCount: filteredCount || totalCount,
-    };
   }
 }
 
