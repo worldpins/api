56496440e4cb7d7054bb5473504b9766a560a324
Jovi De Croock <decroockjovi@gmail.com>
2019-04-01 17:03:00 +0200

    feat: publicMap resolver

--- src/graphql/public/resolvers.js
+++ src/graphql/public/resolvers.js
@@ -3,6 +3,7 @@ const mapService = require('../../services/mapService');
 const mapResolvers = {
   Query: {
     publicMaps: async (obj, args) => mapService.getPublicMaps(args),
+    publicMap: async (obj, args) => mapService.getPublicMap(args.id),
   },
 };
 
